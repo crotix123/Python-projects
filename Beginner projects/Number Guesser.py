8e89579b57934513c6e6fcd8408090e4c024f164
crotix123 <limjingrong456@gmail.com>
2022-09-14 14:56:33 +0800

    Update Number Guesser.py

--- Beginner projects/Number Guesser.py
+++ Beginner projects/Number Guesser.py
@@ -5,14 +5,18 @@ guess = int(input('Guess a number within the range of 1 - 100\n'))
 LowerLimit = 1
 UpperLimit = 100
 
-while guess != ans:
-    if guess > ans:
-        print('Your guess is too high!')
-        UpperLimit = guess
-        guess = int(input('Guess a new number within the range of ' + str(LowerLimit) + ' and ' + str(guess) + '\n'))
-    else:
-        print('Your guess is too low!')
-        LowerLimit = guess
-        guess = int(input('Guess a new number within the range of ' + str(guess) + ' and ' + str(UpperLimit) + '\n'))
+while guess > 100 or guess < 1:
+    guess=int(input(('Choose a number that is between 1-100!!!\n')))
+
 else:
-    print('You Guessed the correct number of ' + str(ans) )
+    while guess != ans:
+        if guess > ans:
+            print('Your guess is too high!')
+            UpperLimit = guess
+            guess = int(input('Guess a new number within the range of ' + str(LowerLimit) + ' and ' + str(guess) + '\n'))
+        else:
+            print('Your guess is too low!')
+            LowerLimit = guess
+            guess = int(input('Guess a new number within the range of ' + str(guess) + ' and ' + str(UpperLimit) + '\n'))
+    else:
+        print('You Guessed the correct number of ' + str(ans) )
